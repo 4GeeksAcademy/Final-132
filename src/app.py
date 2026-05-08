e3ae64681b2c7a51c7e7476a32efdb75b95e5c91
Oscar Becerra <46764350+oskar089@users.noreply.github.com>
2026-05-08 10:52:31 +0000

    Endpoints de atenticacion funcionales

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager
-from flask_bcrypt import Bcrypt
+from extensions import bcrypt
 # from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
@@ -31,6 +31,7 @@ else:
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
+bcrypt.init_app(app)
 app.config["JWT_SECRET_KEY"] = "LOR OF THE RINGS"
 jwt = JWTManager(app)
 # add the admin
